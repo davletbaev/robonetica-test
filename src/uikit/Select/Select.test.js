b57a29271341ebe4ccfdf36fa8db78578965f97c
inferi0n <rda@apptech-team.ru>
2019-08-12 23:36:34 +0300

    fix: remove select component

--- src/uikit/Select/Select.test.js
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-import { shallow } from 'enzyme'
-import Select from './Select'
-
-describe('Select component', () => {
-  it('renders without crashing', () => {
-    shallow(<Select />)
-  });
-})
