d967bb40326821b5662352fd67f957e98b9cffd4
inferi0n <rda@apptech-team.ru>
2019-08-12 23:25:32 +0300

    feature: refactored App component

--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,37 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
+import React from 'react'
+import { shallow, mount } from 'enzyme'
+import App from './App'
 
-it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
-  ReactDOM.unmountComponentAtNode(div);
-});
+describe('App component', () => {
+  it('renders without crashing', () => {
+    shallow(<App />)
+  });
+})
+
+describe('App integration', () => {
+  let component
+
+  beforeEach(() => {
+    component = mount(<App />)
+  })
+
+  afterEach(() => {
+    component.unmount()
+  })
+
+  it('contains Header component', () => {
+    expect(component.exists('Header')).toBeTruthy()
+  })
+
+  it('contains Request Page component', () => {
+    expect(component.exists('Logo')).toBeTruthy()
+  })
+
+  it('contains Nav component', () => {
+    expect(component.exists('Nav')).toBeTruthy()
+  })
+
+  it('contains Request Page component', () => {
+    expect(component.exists('RequestPage')).toBeTruthy()
+  })
+})
