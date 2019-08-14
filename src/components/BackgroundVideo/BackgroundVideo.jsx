408a8c463fb2e3ad6b81bf0046e0a77d5b5d1ff4
inferi0n <rda@apptech-team.ru>
2019-08-15 01:39:44 +0300

    feature: added background video

--- src/components/BackgroundVideo/BackgroundVideo.jsx
+++ src/components/BackgroundVideo/BackgroundVideo.jsx
@@ -1,8 +1,37 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
+import Video from 'react-background-video-player'
+
+import styles from './BackgroundVideo.module.scss'
 
 const BackgroundVideo = () => {
+  const src = "https://video-arn2-1.xx.fbcdn.net/v/t42.9040-2/60854020_1086104511573164_8705388312258412544_n.mp4?_nc_cat=108&efg=eyJ2ZW5jb2RlX3RhZyI6InN2ZV9zZCJ9&_nc_oc=AQnEaFsEiWQemCh_e5rXGebYwkQMin1RBknlbdx5Xi2Hg5il0ArHealv3JreKNOeHAw&_nc_ht=video-arn2-1.xx&oh=0c34648804063d259acd786871f8b6c2&oe=5D544386"
+
+  const [ windowWidth, setWindowWidth ] = useState(window.innerWidth)
+  const [ windowHeight, setWindowHeight ] = useState(window.innerHeight)
+
+  useEffect(() => {
+    function resizeHandler() {
+      setWindowWidth(window.innerWidth)
+      setWindowHeight(window.innerHeight)
+    }
+
+    window.addEventListener('resize', resizeHandler)
+    window.addEventListener('orientationchange', resizeHandler)
+
+    return () => {
+      window.removeEventListener('resize', resizeHandler)
+      window.removeEventListener('orientationchange', resizeHandler)
+    }
+  }, [])
+
   return (
-    <div></div>
+    <div className={ styles['wrap'] }>
+      <Video
+        containerWidth={ windowWidth}
+        containerHeight={ windowHeight }
+        src={ src }
+      />
+    </div>
   )
 }
 
--- src/components/BackgroundVideo/BackgroundVideo.test.js
+++ src/components/BackgroundVideo/BackgroundVideo.test.js
@@ -1,9 +1,23 @@
 import React from 'react'
-import { shallow } from 'enzyme'
+import { shallow, render } from 'enzyme'
 import BackgroundVideo from './BackgroundVideo'
 
 describe('BackgroundVideo component', () => {
+  let wrapper
+
+  beforeEach(() => {
+    wrapper = render(<BackgroundVideo />)
+  })
+
   it('renders without crashing', () => {
     shallow(<BackgroundVideo />)
-  });
+  })
+
+  it('contains video', () => {
+    expect(wrapper.find('video').length).toBe(1)
+  })
+
+  it('contains video with src', () => {
+    expect(wrapper.find('video').attr('src')).toBeTruthy()
+  })
 })
