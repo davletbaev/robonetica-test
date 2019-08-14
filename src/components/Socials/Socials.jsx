7ea499e2de2d6a3d16b2f8420e766b166d76686a
inferi0n <rda@apptech-team.ru>
2019-08-15 01:40:03 +0300

    feature: create Socials component

--- src/components/Socials/Socials.jsx
+++ src/components/Socials/Socials.jsx
@@ -1,9 +1,54 @@
 import React from 'react'
 
+import styles from './Socials.module.scss'
+
+import { ReactComponent as FacebookLogo } from './facebook.svg'
+import { ReactComponent as InstagramLogo } from './instagram.svg'
+import { ReactComponent as LinkedinLogo } from './linkedin.svg'
+
 const Socials = () => {
   return (
-    <div></div>
-  )
+    <ul className={ styles['socials'] }>
+      <li className={ styles['socials__item'] }>
+        <a
+          href="https://facebook.com"
+          rel="noopener noreferrer"
+          target="_blank"
+          title="Facebook"
+          aria-label="Facebook"
+          className={ styles['socials__link'] }
+        >
+          <FacebookLogo />
+        </a>
+      </li>
+
+      <li className={ styles['socials__item'] }>
+        <a
+          href="https://instagram.com"
+          rel="noopener noreferrer"
+          target="_blank"
+          title="Instagram"
+          aria-label="Instagram"
+          className={ styles['socials__link'] }
+        >
+          <InstagramLogo />
+        </a>
+      </li>
+
+      <li className={ styles['socials__item'] }>
+        <a
+          href="https://linkedin.com"
+          rel="noopener noreferrer"
+          target="_blank"
+          title="LinkedIn"
+          aria-label="LinkedIn"
+          className={ styles['socials__link'] }
+        >
+          <LinkedinLogo />
+        </a>
+      </li>
+    </ul>
+  )   
 }
 
 export default Socials
